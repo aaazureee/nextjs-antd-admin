aa380e15a064859ab031e20e2ca627379050e5af
Felt <aaazureee@gmail.com>
2019-08-26 04:39:56 +1000

    fix attempt 2

--- pages/sculptures.js
+++ pages/sculptures.js
@@ -1,4 +1,4 @@
-import { SculptureGrid } from '../components/sculpture-components'
+import SculptureGrid from '../components/sculpture-components/SculptureGrid'
 import Layout from '../components/Layout'
 
 export default () => {
